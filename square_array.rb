9deb858ba0e47c32d9ea3f9c02ac6e24a52f6586
Dustin Blanton <dustinblntn@gmail.com>
2020-05-12 02:02:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,15 @@
-numbers = [1,2,3]
+# numbers = [1,2,3]
 
-def square_array(numbers)
-new_string = []
-numbers.each do |number|
-  new_string << number**2
+# def square_array(numbers)
+# new_string = []
+# numbers.each do |number|
+#   new_string << number**2
 
-end
-new_string
+# end
+# new_string
 
 
-end
+# end
 
 
 
